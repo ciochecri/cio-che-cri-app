38666e680a104964e31eb5997f5865c2336ecea2
Newspada <leonardofacchini1997@gmail.com>
2025-04-12 15:54:49 +0200

    fix

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -108,10 +108,6 @@ export const FooterContainer = styled("div")`
 
   @media screen and (max-width: 769px) {
     width: auto;
-
-    a:not(:last-child) {
-      display: none;
-    }
   }
 
   div {
